0a7020579b1439c64841915f45884d57c0e2995e
nhoknam09877 <118426091+nhoknam09877@users.noreply.github.com>
2025-03-01 08:17:50 +0700

    Add files via upload

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,8 +13,7 @@ cat << EOF > ${DIR_TMP}/heroku.json
         "protocol": "vmess",
         "settings": {
             "clients": [{
-                "id": "${ID}",
-                "alterId": ${AID}
+                "id": "${ID}"
             }]
         },
         "streamSettings": {
@@ -31,16 +30,35 @@ cat << EOF > ${DIR_TMP}/heroku.json
 EOF
 
 # Get V2Ray executable release
+echo "Downloading V2Ray..."
 curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -o ${DIR_TMP}/v2ray_dist.zip
+
+# Check if the download was successful
+if [ ! -f "${DIR_TMP}/v2ray_dist.zip" ]; then
+  echo "Failed to download V2Ray."
+  exit 1
+fi
+
+# Unzip V2Ray
+echo "Unzipping V2Ray..."
 busybox unzip ${DIR_TMP}/v2ray_dist.zip -d ${DIR_TMP}
+
+# Check if unzip was successful
+if [ ! -f "${DIR_TMP}/v2ray" ] || [ ! -f "${DIR_TMP}/v2ctl" ]; then
+  echo "Failed to unzip V2Ray."
+  exit 1
+fi
 
 # Convert to protobuf format configuration
 mkdir -p ${DIR_CONFIG}
 ${DIR_TMP}/v2ctl config ${DIR_TMP}/heroku.json > ${DIR_CONFIG}/config.pb
 
 # Install V2Ray
+echo "Installing V2Ray..."
 install -m 755 ${DIR_TMP}/v2ray ${DIR_RUNTIME}
+install -m 755 ${DIR_TMP}/v2ctl ${DIR_RUNTIME}
 rm -rf ${DIR_TMP}
 
 # Run V2Ray
+echo "Starting V2Ray..."
 ${DIR_RUNTIME}/v2ray -config=${DIR_CONFIG}/config.pb
